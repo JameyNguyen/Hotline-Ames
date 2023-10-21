b065c786ffbbbf849fe848ba7352d1da2ae975ed
Jamey Nguyen <asiandeadly5@gmail.com>
2023-10-21 18:49:45 -0500

    adding player functions
    the player can get money, take damage, and use action pts

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    int health;
-    string name;
-    int currency;
-    int level;
-    int actionPoint;
+    int health = 150;
+    int currency = 0;
+    int actionPoint = 5;
 
+    public void gainCurrency(int gold) 
+    {
+        currency += gold;
+    }
 
+    public void takeDamage(int damage)
+    {
+        // maybe either here or some other object calls this function based on card conditionls
+        health -= damage;
+        
+    }
 
+    public void useActionPoints(int point)
+    {
+        if (actionPoint - point < 0)
+        {
+            // not enough points to do action
+            return;
+        }
+        actionPoint -= point;
+    }
 }
