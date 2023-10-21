fc317e3970928703f439c321e62348c1bcee8332
Jamey Nguyen <asiandeadly5@gmail.com>
2023-10-21 16:43:35 -0500

    big cocka moment

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -8,7 +8,8 @@ public class NewBehaviourScript : MonoBehaviour
     public List<Card> deck = new List<Card>();
     public Transform[] cardSlots;
     public bool[] availableCards;
-
+    public List<Card> selectedCards = new List<Card>();
+ 
     public void drawCard()
     { 
         if (deck.Count >= 1)
@@ -17,15 +18,30 @@ public class NewBehaviourScript : MonoBehaviour
 
             for (int i = 0; i < availableCards.Length; i++)
             {
-                if (availableCards[i] == true)
-                {
-                    randCard.gameObject.SetActive(true);
-                    randCard.transform.position = cardSlots[i].position;
-                    availableCards[i] = false;
-                    deck.Remove(randCard);
-                    return;
-                }
+                 randCard.gameObject.SetActive(true);
+                 randCard.transform.position = cardSlots[i].position;
+                 availableCards[i] = false;
+                 deck.Remove(randCard);
+                 return;
             }
         }
     }
+
+    public void shuffle()
+    {
+        for (int i = 0; i <= 50; i++)
+        {
+            int j = Random.Range(0, 51);
+            Card temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+    }
+
+    public void selectCard(Card card)
+    { 
+        selectedCards.Add(card);
+    }
+
+
 }
