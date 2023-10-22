9c43995bdfef7ea38e3b8f655b3b0ec7432e103d
Peachie-Codinson <buinhatanh309@gmail.com>
2023-10-22 17:52:29 -0500

    Revert "Update SampleScene.unity"
    This reverts commit 6dc587d8c05bd35b06682136fd46ce04a465ab25.

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
+    int maxHealth = 150;
     int health = 150;
-    
+    public Text text;
+    public Text lastActionText;
+    Player player;
+    EventManager manager;
+
     public void takeDamage(int damage)
     {
         // maybe either here or some other object calls this function based on card conditionls
@@ -15,5 +21,68 @@ public class Unit : MonoBehaviour
             // gg lol
         }
 
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public void attackPlayer()
+    {
+        int highestVal = 0;
+        foreach (Card card in manager.community)
+        {
+            if (card.value >= highestVal)
+            {
+                highestVal = card.value;
+            }
+        }
+        player.takeDamage(highestVal);
+        manager.changeState(GameState.PLAYERTURN);
+    }
+
+    public void healSelf()
+    {
+        int highestVal = 0;
+        foreach (Card card in manager.community)
+        {
+            if (card.value >= highestVal)
+            {
+                highestVal = card.value;
+            }
+        }
+        health += highestVal;
+        // can't let enemy go above max hp lol
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        manager.changeState(GameState.PLAYERTURN);
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        manager = FindObjectOfType<EventManager>();
+    }
+    private void Update()
+    {
+        text.text = getHealth().ToString();
+
+        if (manager.getState() == GameState.ENEMYTURN)
+        {
+            int randomChoice = Random.Range(0, 2);
+            if (randomChoice == 0)
+            {
+                lastActionText.text = "Attacked";
+                attackPlayer();
+            }
+            if (randomChoice == 1)
+            {
+                lastActionText.text = "Healed";
+                healSelf();
+            }
+        }
     }
 }
