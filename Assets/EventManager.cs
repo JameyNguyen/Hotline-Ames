ad6e865ae64f7e154199ba0f63bd34e8fe8f890d
Peachie-Codinson <buinhatanh309@gmail.com>
2023-10-22 17:38:02 -0500

    fuckin hell

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using TMPro;
+using UnityEngine.UI;
 
 public class EventManager : MonoBehaviour
 {
@@ -11,7 +13,12 @@ public class EventManager : MonoBehaviour
     public List<Card> selectedCards = new List<Card>();
     public Transform discardSlot;
     public Card[] community = new Card[5];
- 
+    public Text handText;
+    public string handreturn = "";
+
+
+
+
     public void drawCard()
     { 
         if (deck.Count >= 1)
@@ -21,7 +28,7 @@ public class EventManager : MonoBehaviour
                 if (availableCards[i] == true)
                 {
 
-                    Card randCard = deck[Random.Range(0, deck.Count)];
+                    Card randCard = deck[UnityEngine.Random.Range(0, deck.Count)];
                     randCard.gameObject.SetActive(true);
                     randCard.transform.position = cardSlots[i].position;
                     randCard.index = i;
@@ -32,7 +39,7 @@ public class EventManager : MonoBehaviour
                 }
             }
         }
-
+        handText.text = checkHand(community);
         return;
     }
 
@@ -44,8 +51,8 @@ public class EventManager : MonoBehaviour
             {
                 selectedCards[i].transform.position = discardSlot.position;
                 availableCards[selectedCards[i].index] = true;
-                selectedCards[i].index = -1;
                 community[selectedCards[i].index] = null;
+                selectedCards[i].index = -1;
             }
         }
         while (selectedCards.Count > 0)
@@ -55,15 +62,10 @@ public class EventManager : MonoBehaviour
         drawCard();
     }
 
-    public void shuffle()
+
+    public string getHand()
     {
-        for (int i = 0; i <= 50; i++)
-        {
-            int j = Random.Range(0, 51);
-            Card temp = deck[i];
-            deck[i] = deck[j];
-            deck[j] = temp;
-        }
+        return handreturn;
     }
 
     public void selectCard(Card card)
@@ -76,4 +78,226 @@ public class EventManager : MonoBehaviour
         selectedCards.Remove(card);
     }
 
+    public string checkHand(Card[] checkHand)
+    {
+        if (check_straight_flush(checkHand))
+        {
+            return "straight flush";
+        }
+        else if (check_four_of_a_kind(checkHand))
+        {
+            return "four of a kind";
+        }
+        else if (check_full_house(checkHand))
+        {
+            return "full house";
+        }
+        else if (check_flush(checkHand))
+        {
+            return "Flush";
+        }
+        else if (check_straight(checkHand))
+        {
+            return "straight";
+        }
+        else if (check_three_of_a_kind(checkHand))
+        {
+            return "three of a kind";
+        }
+        else if (check_three_of_a_kind(checkHand))
+        {
+            return "three of a kind";
+        }
+        else if (check_two_pairs(checkHand))
+        {
+            return "two pairs";
+        }
+        else if (check_one_pairs(checkHand))
+        {
+            return "one pair";
+        }
+        else
+        {
+            return  "high card";
+        }
+    }
+
+
+    private bool check_straight_flush(Card[] str8FlHand)
+    {
+        if (check_flush(str8FlHand) && check_straight(str8FlHand))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    private bool check_four_of_a_kind(Card[] hand)
+    {
+        Dictionary<int, int> valTally = new Dictionary<int, int>();
+        foreach (Card c in hand)
+        {
+            if (valTally.ContainsKey(c.value))
+            {
+                valTally[c.value] += 1;
+            }
+            else
+            {
+                valTally[c.value] = 1;
+            }
+        }
+        if (valTally.ContainsValue(4))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    private bool check_full_house(Card[] hand)
+    {
+        Dictionary<int, int> valTally = new Dictionary<int, int>();
+        foreach (Card c in hand)
+        {
+            if (valTally.ContainsKey(c.value))
+            {
+                valTally[c.value] += 1;
+            }
+            else
+            {
+                valTally[c.value] = 1;
+            }
+        }
+        if (valTally.ContainsValue(3) && valTally.ContainsValue(2))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    private bool check_flush(Card[] hand)
+    {
+        Dictionary<char, int> valTally = new Dictionary<char, int>();
+        foreach (Card c in hand)
+        {
+            if (valTally.ContainsKey(c.suit))
+            {
+                valTally[c.suit] += 1;
+            }
+            else
+            {
+                valTally[c.suit] = 1;
+            }
+        }
+        if (valTally.ContainsValue(5))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    private bool check_straight(Card[] hand)
+    {
+        HashSet<int> vals = new HashSet<int>();
+        int ma = 0;
+        int mi = 15;
+        int temp;
+        foreach (Card c in hand)
+        {
+            if (vals.Contains(c.value))
+            {
+                return false;
+            }
+            temp = c.value;
+            vals.Add(temp);
+            ma = Math.Max(ma, temp);
+            mi = Math.Min(mi, temp);
+        }
+        if (ma - mi == 4)
+        {
+            return true;
+        }
+        if (vals.Contains(2) && vals.Contains(3) && vals.Contains(4) && vals.Contains(5) && vals.Contains(14))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    private bool check_three_of_a_kind(Card[] hand)
+    {
+        Dictionary<int, int> valTally = new Dictionary<int, int>();
+        foreach (Card c in hand)
+        {
+            if (valTally.ContainsKey(c.value))
+            {
+                valTally[c.value] += 1;
+            }
+            else
+            {
+                valTally[c.value] = 1;
+            }
+        }
+        if (valTally.ContainsValue(3))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    private bool check_two_pairs(Card[] hand)
+    {
+        Dictionary<int, int> valTally = new Dictionary<int, int>();
+        foreach (Card c in hand)
+        {
+            if (valTally.ContainsKey(c.value))
+            {
+                valTally[c.value] += 1;
+            }
+            else
+            {
+                valTally[c.value] = 1;
+            }
+        }
+        int cnt = 0;
+        foreach (KeyValuePair<int, int> entry in valTally)
+        {
+            if (entry.Value == 2)
+            {
+                cnt += 1;
+            }
+        }
+        return cnt == 2;
+    }
+
+
+    private bool check_one_pairs(Card[] hand)
+    {
+        Dictionary<int, int> valTally = new Dictionary<int, int>();
+
+        foreach (Card c in hand)
+        {
+            if (valTally.ContainsKey(c.value))
+            {
+                valTally[c.value] += 1;
+            }
+            else
+            {
+                valTally[c.value] = 1;
+            }
+        }
+        if (valTally.ContainsValue(2))
+        {
+            return true;
+        }
+        return false;
+    }
 }
--- Assets/handret.cs
+++ Assets/handret.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class handret : MonoBehaviour
 {
+    private Card[] currhand;
+    private EventManager manager;
+    public Text handOut;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = FindObjectOfType<EventManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    
 }
